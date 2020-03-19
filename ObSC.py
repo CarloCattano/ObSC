61570805404f5667e374a0f25cf1909a551eda76
Carlo Cattano <cattanoaudioproduction@gmail.com>
2020-03-19 18:42:29 +0100

    uploaded updated script

---
+++ ObSC.py
@@ -0,0 +1,71 @@
+import sys,time,argparse,math
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+sys.path.append('../')
+from obswebsocket import obsws, requests  # noqa: E402
+
+host = "localhost"
+port = 4444
+password = "secret"
+
+ws = obsws(host, port, password)
+ws.connect()
+
+ScenesNames = []
+SceneSources = []
+
+def sourceSwitch(source_name,scene,switch):
+    ws.call(requests.SetSceneItemProperties(source_name,scene,visible=switch))
+
+def scene_switch(unused_addr, args, filter):
+    if filter > 60 :
+        #ws.call(requests.SetCurrentScene(ScenesNames[1]))
+        sourceSwitch("webcam1","Scene1",False)
+        sourceSwitch("screen1","Scene1",True)
+        sourceSwitch("screen2","Scene1",False)
+    elif filter <= 59 :
+        #ws.call(requests.SetCurrentScene(ScenesNames[0]))
+        sourceSwitch("webcam1","Scene1",True)
+        sourceSwitch("screen1","Scene1",False)
+        sourceSwitch("screen2","Scene1",True)
+    print("[{0}] +" " + {1}".format(args[0], filter))    
+
+
+if __name__ == "__main__":
+
+    try:
+        scenes = ws.call(requests.GetSceneList())
+
+        for s in scenes.getScenes():
+            name = s['name']
+            print(ws.call(requests.GetSourcesList()))
+            print(name)
+            ScenesNames.append(name)
+
+        print(ScenesNames)
+       
+        ### OSC SETTINGS
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--ip",default="127.0.0.1", help="The ip to listen on")
+        parser.add_argument("--port",type=int, default=5005, help="The port to listen on")
+        
+        args = parser.parse_args()
+        dispatcher = dispatcher.Dispatcher()
+
+        ## OSC LISTENER
+        dispatcher.map("/Scene", scene_switch, "Scene")
+        server = osc_server.ThreadingOSCUDPServer(
+            (args.ip, args.port), dispatcher)
+
+        print("Serving on {}".format(server.server_address))
+        
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        pass
+
+    ws.disconnect()
